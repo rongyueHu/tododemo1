da15c78b72f5b63f043d0185f38e3d929f44f9bf
rongyueHu <1752333046@qq.com>
2022-09-18 01:08:33 +0800

    增加功能已完成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="todoapp">
     <!-- 除了驼峰, 还可以使用-转换链接 -->
-    <TodoHeader></TodoHeader>
+    <TodoHeader @add-event="addtodolist"></TodoHeader>
     <TodoMain :list="list" @del-event="deltodolist"></TodoMain>
     <TodoFooter></TodoFooter>
   </section>
@@ -29,9 +29,19 @@ export default {
     }
   },
   methods: {
+    //删除事件
     deltodolist (Id) {
       const index = this.list.findIndex(item => item.id === Id)
       this.list.splice(index, 1)
+    },
+    //添加事件
+    addtodolist (taskname) {
+      let id = this.list.length === 0 ? 100 : this.list[this.list.length - 1].id + 1
+      this.list.push({
+        id: id,
+        name: taskname,
+        isDone: false
+      })
     }
   },
   components: {
--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -3,12 +3,28 @@
     <h1>todos</h1>
     <input id="toggle-all" class="toggle-all" type="checkbox" />
     <label for="toggle-all"></label>
-    <input class="new-todo" placeholder="输入任务名称-回车确认" autofocus />
+    <input
+      v-model="task"
+      class="new-todo"
+      placeholder="输入任务名称-回车确认"
+      autofocus
+      @keydown.enter="add(task)"
+    />
   </header>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      task: ''
+    }
+  },
+  methods: {
+    add () {
+      this.$emit("add-event", this.task)
+      this.task = ''
+    }
+  }
 }
 </script>
